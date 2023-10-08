0275f05271867970981021bb1937ae5671d390d0
somaftees <somaftees@gmail.com>
2023-10-08 09:33:23 -0700

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     x = ''
     for i in range(len(my_string)):
-        if i != 'c' and i != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             x += my_string[i]
     return x
