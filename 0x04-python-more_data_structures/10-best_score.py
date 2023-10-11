aa1142209d6eee47d2529fc698dca6374f4451fb
somaftees <somaftees@gmail.com>
2023-10-11 14:29:38 -0700

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict and len(my_dict):
-        m = list(my_dict.keys())[0]
+        max = list(my_dict.keys())[0]
         for key in my_dict:
             if my_dict[key] > my_dict[max]:
-                m = key
-        return m
+                max = key
+        return max
     return None
