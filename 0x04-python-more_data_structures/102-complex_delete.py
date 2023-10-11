afc33d271ccbb07ffff7c57386eb2e165198e3ca
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2023-10-11 23:35:31 +0200

    done

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(my_dict, value):
+    de_keys = []
+    for key in my_dict:
+        if my_dict[key] == value:
+            del_keys.append(key)
+    for key in del_keys:
+        del my_dict[key]
+    return my_dict
