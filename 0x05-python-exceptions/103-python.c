68f1a23b9e004c955ab6164f191b55567997fc6a
somaftees <somaftees@gmail.com>
2023-10-23 14:42:29 -0700

    done

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - Python lists.
+ * @p: PyObject.
+ * Return: void.
+ */
+
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+ * print_python_bytes - Python bytes.
+ * @p: PyObject.
+ * Return: void.
+ */
+
+void print_python_bytes(PyObject *p)
+{
+}
+
+/**
+ * print_python_float - Python float.
+ * @p: PyObject.
+ * Return: void.
+ */
+
+void print_python_float(PyObject *p)
+{
+}
