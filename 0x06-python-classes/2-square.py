99c9c5c66c4c3217cb65d83433239b900a3015cc
somaftees <somaftees@gmail.com>
2023-10-24 06:50:39 -0700

    done

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a squre class"""
+
+class Square:
+    """squre"""
+    def __init__(self, size=0):
+        """init a square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
