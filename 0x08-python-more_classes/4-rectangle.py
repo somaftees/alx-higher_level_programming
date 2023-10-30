c747d579a070c60f0b33d672a24f4872c024423d
somaftees <somaftees@gmail.com>
2023-10-30 12:05:06 -0700

    done

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -56,6 +56,7 @@ class Rectangle:
             string += "\n".join("#" * self.__width
                                 for j in range(self.__height))
         return string
+
     def __repr__(self):
         """reproduction rectangle"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
