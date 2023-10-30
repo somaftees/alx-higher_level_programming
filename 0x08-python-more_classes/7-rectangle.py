539d7984b298e1df8e25c631469d4d2735ecb5ad
somaftees <somaftees@gmail.com>
2023-10-30 12:42:43 -0700

    done

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     """Rectangle class"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
@@ -63,7 +64,7 @@ class Rectangle:
         """str rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
