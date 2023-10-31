72a6cd9e9b10919c33369cac8b26921dbfb9bf52
somaftees <somaftees@gmail.com>
2023-10-31 11:10:32 -0700

    done

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""matrix_mult fun"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """matrix_mul"""
+    return (np.matmul(m_a, m_b))
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""indentation text"""
+
+
+def text_indentation(text):
+    """text indentation"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
