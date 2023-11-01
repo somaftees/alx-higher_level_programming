ef1daf9b799bfdee546bba2e461d3eb392c79991
somaftees <somaftees@gmail.com>
2023-11-01 04:06:23 -0700

    done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,3 +10,8 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if size > 0:
         print(("#" * size + "\n") * size, end="")
+
+
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile("tests/4-print_square.txt")
