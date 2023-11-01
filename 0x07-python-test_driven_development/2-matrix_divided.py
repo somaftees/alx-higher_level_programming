e6f34272c44a3b6ad951e10dd7f6f6c3b14fc1cc
somaftees <somaftees@gmail.com>
2023-11-01 04:00:51 -0700

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -27,3 +27,8 @@ def matrix_divided(matrix, div):
     new_matrix = list(map(lambda row:
                           list(map(lambda x: round(x/div, 2), row)), matrix))
     return new_matrix
+
+
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile("tests/2-matrix_divided.txt")
