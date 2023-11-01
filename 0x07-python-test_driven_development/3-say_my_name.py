b403409c7eeb51c1777ad71e13cfd4ad97010b34
somaftees <somaftees@gmail.com>
2023-11-01 04:04:35 -0700

    done

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """say my name """
 
+
 def say_my_name(first_name, last_name=""):
     """Prints "My name is" followed by the first name and optional last name"""
     if type(first_name) is not str:
@@ -8,3 +9,8 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is", first_name, last_name)
+
+
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile("tests/3-say_my_name.txt")
