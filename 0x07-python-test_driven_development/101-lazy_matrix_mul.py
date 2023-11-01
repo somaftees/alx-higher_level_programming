2c14a1ad021698d90ed1259438f5b5c95b3b91a2
somaftees <somaftees@gmail.com>
2023-11-01 03:55:13 -0700

    done

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,3 +6,8 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """matrix_mul"""
     return (np.matmul(m_a, m_b))
+
+
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile("tests/101-lazy_matrix_mul.txt")
