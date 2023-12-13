81b91aea290de8f3d9687c62d6a25558e0d14b7a
somaftees <somaftees@gmail.com>
2023-12-13 13:29:30 -0800

    done

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,14 @@
+-- a script that uses the hbtn_0d_tvshows
+
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN (
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
