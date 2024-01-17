f3f0d41ba5d73d558fcd4de3f21699fa6a34e6a1
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-01-17 18:45:01 +0200

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-A script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
+"""a script that lists all states with a name starting with N"""
 import MySQLdb
 import sys
 
