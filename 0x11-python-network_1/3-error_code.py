083b6652e71db7b33357d061fe83f585646a024f
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-01-26 15:38:35 +0200

    done

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""sends a request to the URL and displays the body"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
