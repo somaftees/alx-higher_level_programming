3d657f9865e9d38975b8362849ddde85fcd51c30
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-01-26 15:56:33 +0200

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""takes your GitHub credentials 
-(username and password) 
-and uses the GitHub API to display your id
-"""
+"""GitHub."""
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
